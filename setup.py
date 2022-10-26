195975b60fb57897e6a7899f7a4f25ea3e28ffbe
Jefferson Andrade <jeffersonjla@hotmail.com>
2022-10-26 10:53:45 -0300

    iniciando

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
+    name="imagem_processamento",
     version="0.0.1",
     author="Jefferson Andrade",
     author_email="jeffersonjla@hotmail.com",
